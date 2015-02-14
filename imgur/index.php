7d91eae9e3c615a376a10658e906157f46bbd193
Derek Hoagland <thegrickit@gmail.com>
2015-02-14 14:34:19 -0500

    Force jpeg to save my bandwidth. Don't include the extension in the E-Tag. webm support is too complicated for right now.

--- imgur/index.php
+++ imgur/index.php
@@ -1,22 +1,25 @@
 <?php
-  if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
-    header('HTTP/1.1 304 Not Modified');
-  }
 
-  elseif(isset($_GET['imageID']) && preg_match('/^([a-zA-Z0-9]{3,9})(.png|.gif|.jpg|.jpeg|.webm|.gifv)$/',$_GET['imageID'])) {
-    $imageID = $_GET['imageID'];
-    // Perform additional processing?
-
-    $contents = file_get_contents("http://imgur.com/${imageID}");
-    $length = strlen($contents);
+  function outputHeaders($imageID,$length) {
     $timestamp = gmstrftime("%a, %d %b %Y %T %Z",time());
     $expires = gmstrftime("%a, %d %b %Y %T %Z",time()+31536000);
 
-    header("Content-Type: image/png");
     header("Content-Length: ${length}");
+    header("Content-Type: image/jpeg");
     header("Cache-Control: public, max-age=31536000");
     header("Expires: ${expires}");
     header("Last-Modified: ${timestamp}");
     header("E-Tag: ${imageID}_".time());
+  }
+
+  if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
+    header('HTTP/1.1 304 Not Modified');
+  }
+
+  elseif(isset($_GET['imageID']) && preg_match('/^([a-zA-Z0-9]{3,9})$/',$_GET['imageID'],$matches)) {
+    $imageID = $matches[1];
+
+    $contents = file_get_contents("http://i.imgur.com/${imageID}.jpeg");
+    outputHeaders($imageID,strlen($contents));
     echo $contents;
   }
