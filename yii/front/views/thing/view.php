a45bbed6b4930ace6cdfd7c5926e6759fb03ef9b
Derek Hoagland <thegrickit@gmail.com>
2015-03-08 00:56:27 -0500

    Quick fix to responsiveness on the view view

--- yii/front/views/thing/view.php
+++ yii/front/views/thing/view.php
@@ -10,16 +10,15 @@
     <div class="article-banner" style="background-image: url('<?php echo StaticURL::make('images/thing/banner',$model->nameSafe.'.png'); ?>');">
         <div class="article-info">
             <h1><?php echo Html::encode($this->title); ?></h1>
-            <div class="article-meta">
-                <?php echo Html::a('Project homepage', $model->linkURL, array('target' => '_blank')); ?>
-                <br>
-                <?php echo ($model->activeStatus ? '<span class="label label-success">Status: Active</span>' : '<span class="label label-default">Status: Inactive</span>'); ?>
-            </div>
         </div>
         <?php echo $this->render('_like',['model' => $model, 'like' => $like]); ?>
     </div>
 
     <div class="article">
+        <ul class="article-meta">
+            <li>Status: <?php echo ($model->activeStatus ? '<span class="label label-success">Active</span>' : '<span class="label label-default">Inactive</span>'); ?></li>
+            <li>Homepage: <?php echo Html::a('click here', $model->linkURL, array('target' => '_blank')); ?></li>
+        </ul>
         <?php echo HtmlPurifier::process($model->description); ?>
         <p>Last updated: <?php echo $model->updateDate; ?> UTC</p>
     </div>
