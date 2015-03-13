23fb68cf6b26606d8fcc0e1dbf19dbd4615aa438
Derek Hoagland <thegrickit@gmail.com>
2015-03-12 23:09:45 -0400

    Tag model because I should get at least some tiny thing done tonight.

---
+++ yii/front/models/tag.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace front\models;
+
+use Yii;
+use common\components\SafeName;
+
+class tag extends \yii\db\ActiveRecord {
+
+    public static function tableName() {
+        return 'front_tags';
+    }
+
+    public function rules() {
+        return [];
+    }
+
+    public function attributeLabels() {
+        return [
+            'id' => 'ID',
+            'createDate' => 'Created',
+            'updateDate' => 'Last Updated',
+            'name' => 'Name',
+            'nameSafe' => 'Internal Name'
+            'modelType' => 'Type',
+            'modelID' => 'Name'
+        ];
+    }
+
+    public function init() {
+        $this->createDate = date('Y-m-d H:i:s',time());
+        $this->updateDate = date('Y-m-d H:i:s',time());
+
+        return parent::init();
+    }
+
+    public function beforeSave($insert) {
+        if($this->scenario === 'update') {
+            $this->updateDate = date('Y-m-d H:i:s',time());
+        }
+        elseif($this->scenario === 'create') {
+            $this->nameSafe = SafeName::make($this->name);
+        }
+
+        return parent::beforeSave($insert);
+    }
+
+}
