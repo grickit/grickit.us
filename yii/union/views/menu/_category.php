e865988505c8dc2a631afc6a2bc8010e319b1366
Derek Hoagland <thegrickit@gmail.com>
2015-03-29 12:46:57 -0400

    Floating prices don't work for responsive.

--- yii/union/views/menu/_category.php
+++ yii/union/views/menu/_category.php
@@ -6,7 +6,7 @@
 
   <a class="category tile" href="<?php echo Yii::$app->urlManager->createURL("menu/".$model->nameSafe); ?>">
     <span class="category title"><?php echo $model->name; ?></span>
-    <span class="priceGlobal"><?php if($model->priceGlobal) { echo "All available for ".$model->formattedPrice; } ?></span>
+    <span class="priceGlobal"><?php if($model->priceGlobal) { echo " all available for ".$model->formattedPrice; } ?></span>
     <span class="notes"><?php echo $model->notes; ?></span>
   </a>
 
