f3c60e0d238addda20f390170eb5ee5f576e3b8d
Derek Hoagland <thegrickit@gmail.com>
2015-04-04 16:29:23 -0400

    Responsive design

--- buttnet/index.php
+++ buttnet/index.php
@@ -1,7 +1,14 @@
-<html>
-  <body style="background-color: #E6CFE0; color: #444444; text-align: center; padding: 50px">
-    <img src="http://static.grickit.us/images/buttnet/logo.png">
-    <br>
-    Coming soon.
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+      <meta charset="UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Buttnet: Landing</title>
+  </head>
+  <body style="background-color: #E6CFE0; color: #444444; text-align: center;">
+    <div style="padding: 10px; display: inline-block;">
+      <img src="http://static.grickit.us/images/buttnet/logo.png" style="width: 100%; max-width: 500px; height: auto;">
+      <p>Coming soon.</p>
+    </div>
   </body>
 </html>
