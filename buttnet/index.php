6e1be2d041a2c4a4a97cfe8c56fcf2b01157b03e
Derek Hoagland <thegrickit@gmail.com>
2015-04-04 16:02:15 -0400

    Good game mate

--- buttnet/index.php
+++ buttnet/index.php
@@ -1,6 +1,6 @@
 <html>
   <body style="background-color: #E6CFE0; color: #444444; text-align: center; padding: 50px">
-    <img src="http://static.local.host/images/buttnet/logo.png">
+    <img src="http://static.grickit.us/images/buttnet/logo.png">
     <br>
     Coming soon.
   </body>
