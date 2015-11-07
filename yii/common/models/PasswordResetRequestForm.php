32ef8e2387095bb827decf0b351684c24e1d833e
Derek Hoagland <thegrickit@gmail.com>
2015-11-07 13:06:16 -0500

    One day we might have a mail server

---
+++ yii/common/models/PasswordResetRequestForm.php
@@ -0,0 +1,49 @@
+<?php
+namespace common\models;
+
+use common\models\User;
+use yii\base\Model;
+
+/**
+ * Password reset request form
+ */
+class PasswordResetRequestForm extends Model
+{
+    public $email;
+
+     public function rules() {
+        return [
+            ['email', 'filter', 'filter' => 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'exist',
+                'targetClass' => '\common\models\User',
+                'filter' => ['statusActive' => User::STATUS_ACTIVE],
+                'message' => 'There is no user with such email.'
+            ],
+        ];
+    }
+
+    public function sendEmail() {
+        $user = User::findOne([
+            'statusActive' => User::STATUS_ACTIVE,
+            'email' => $this->email,
+        ]);
+
+        if ($user) {
+            if (!User::isPasswordResetTokenValid($user->resetKey)) {
+                $user->generatePasswordResetToken();
+            }
+
+            if ($user->save()) {
+                return \Yii::$app->mailer->compose(['html' => 'passwordResetToken-html', 'text' => 'passwordResetToken-text'], ['user' => $user])
+                    ->setFrom([\Yii::$app->params['adminEmail'] => \Yii::$app->name . ' robot'])
+                    ->setTo($this->email)
+                    ->setSubject('Password reset for ' . \Yii::$app->name)
+                    ->send();
+            }
+        }
+
+        return false;
+    }
+}
